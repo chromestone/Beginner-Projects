e6cffd7630edb43ff613fb6ebb5eaad7b89e8736
Derek Zhang <derekzhang@Dereks-MBP.fios-router.home>
2015-12-26 11:39:49 -0500

    OddFinder
    The program that finds odd numbers from 1-100.

---
+++ OddFinder/OddFinder.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.util.*;
+
+public class OddFinder {
+   public static void main(String args[]) {
+      System.out.println("runningOddFinder...");
+      for (int a=1; a<101; a++) {
+         if (a % 2 == 1) {
+               System.out.println(a);
+         }
+         else {
+         }
+
+      }
+   }
+}
